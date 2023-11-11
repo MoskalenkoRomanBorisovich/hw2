02965c0b55b6c3575c33184de3ed388e2e09f58b
Roman <romichbor@rambler.ru>
2023-11-11 17:54:27 +0300

    changes

--- compile.sh
+++ compile.sh
@@ -3,7 +3,11 @@ mkdir -p build
 
 module load nvidia_sdk/nvhpc/23.5
 
+echo compiling cuda
 nvcc -O3 main.cu -lcublas -o build/main_cu
+echo complete
 
+echo compiling openmp
 nvc++ -mp=gpu -O3 main_omp.c -o build/main_omp
+echo complete
 # nvc -O3 main.c -o build/main_omp
