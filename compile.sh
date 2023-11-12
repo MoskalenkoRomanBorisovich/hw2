db711108464d68478b90b3fc99d25c772727e1f0
Roman <romichbor@rambler.ru>
2023-11-12 22:35:14 +0300

    benchmark results

--- compile.sh
+++ compile.sh
@@ -8,6 +8,6 @@ nvcc -O3 main.cu -lcublas -o build/main_cu
 echo complete
 
 echo compiling openmp
-nvc++ -mp=gpu -O3 main_omp.c -o build/main_omp
+nvc++ -mp=gpu -O3 main_omp.cpp -o build/main_omp
 echo complete
 # nvc -O3 main.c -o build/main_omp
